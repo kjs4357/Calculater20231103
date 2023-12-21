31f936abecfeb6ad766af3fd86ef31230c0f7240
지수 <jsjs4357@naver.com>
2023-12-22 00:47:02 +0900

    이슈수정 5: 새로운 연산 +/-변환기능추가

--- calculator_main (1).py
+++ calculator_main (1).py
@@ -66,6 +66,9 @@ class Main(QDialog):
         button_reciprocal = QPushButton("1/x")
         button_square = QPushButton("x^2")
         button_square_root = QPushButton("√x")
+        button_sign_change = QPushButton("±")
+
+        layout_operation.addWidget(button_sign_change)
         layout_special_functions.addWidget(button_percent)
         layout_special_functions.addWidget(button_clear_entry)
         layout_special_functions.addWidget(button_clear)
@@ -73,7 +76,8 @@ class Main(QDialog):
         layout_special_functions.addWidget(button_square)
         layout_special_functions.addWidget(button_square_root)
         main_layout.addLayout(layout_special_functions)
-
+        layout_operation.addWidget(button_square_root)
+        button_sign_change.clicked.connect(self.button_sign_change_clicked)
 
         # 특수 기능 버튼 이벤트 연결
         button_percent.clicked.connect(self.button_percent_clicked)
@@ -82,6 +86,9 @@ class Main(QDialog):
         button_reciprocal.clicked.connect(self.button_reciprocal_clicked)
         button_square.clicked.connect(self.button_square_clicked)
         button_square_root.clicked.connect(self.button_square_root_clicked)
+    
+       
+
 
         ### 숫자 버튼 생성하고, layout_number 레이아웃에 추가
         ### 각 숫자 버튼을 클릭했을 때, 숫자가 수식창에 입력 될 수 있도록 시그널 설정
@@ -182,6 +189,20 @@ class Main(QDialog):
             self.solution.setText(str(current_value ** 0.5))
         except ValueError as e:
             self.solution.setText(str(e))
+        except Exception as e:
+            self.solution.setText("Error")
+
+    def button_sign_change_clicked(self):
+        try:
+            # 현재 수식창의 텍스트를 가져온다.
+            current_value = self.equation.text()
+            # 첫 문자가 '-'인 경우, 이를 제거한다.
+            if current_value.startswith('-'):
+                self.equation.setText(current_value[1:])
+            # 그렇지 않고, 문자열이 비어있지 않거나 '0'이 아닌 경우, '-'를 앞에 추가한다.
+            elif current_value and current_value != '0':
+                self.equation.setText('-' + current_value)
+            # 그 외의 경우는 텍스트를 변경하지 않는다.
         except Exception as e:
             self.solution.setText("Error")
 
